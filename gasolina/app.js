ce8fd14dc8c2473894f6db4329b06c1f05fe023f
KevinXirum <xirumkevin@gmail.com>
2021-10-22 19:29:45 -0600

    Nuevos updates

---
+++ gasolina/app.js
@@ -0,0 +1,30 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyCL5skhwb8FQ4ZBY2RDq2Qg7DT1AnSSYXk",
+    authDomain: "paginasistema-88636.firebaseapp.com",
+    projectId: "paginasistema-88636"
+});
+
+var db = firebase.firestore();
+
+function guardar() {
+    //var fecha = document.getElementById('fecha').value;
+    //var camion = document.getElementById('camion').value;
+    var precio = document.getElementById('precio').value;
+    var cantidad = document.getElementById('cantidad').value;    
+    var coments = document.getElementById('fecha').value;
+
+    db.collection("gasolina").add({
+        //date: fecha,
+        //veiculo: camion,
+        price: precio,
+        amount: cantidad,
+        more: coments
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
